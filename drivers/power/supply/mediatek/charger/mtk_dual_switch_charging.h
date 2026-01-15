d2a1a94c665cd58f22b4997e843879def5f64712
xG4nP <xG4nP@users.noreply.github.com>
2026-01-15 07:42:35 +0000

    Complete Source: aemeath-sk 4.19

---
+++ drivers/power/supply/mediatek/charger/mtk_dual_switch_charging.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+*/
+
+#ifndef _MTK_DUAL_SWITCH_CHARGER_H
+#define _MTK_DUAL_SWITCH_CHARGER_H
+
+struct dual_switch_charging_alg_data {
+	int state;
+	bool disable_charging;
+	struct mutex ichg_aicr_access_mutex;
+
+	unsigned int total_charging_time;
+	unsigned int pre_cc_charging_time;
+	unsigned int cc_charging_time;
+	unsigned int cv_charging_time;
+	unsigned int full_charging_time;
+	struct power_supply	*usb_psy;
+	int	vbus_mv;
+	bool first_run;
+};
+
+#endif /* End of _MTK_DUAL_SWITCH_CHARGER_H */
